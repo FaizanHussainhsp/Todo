1363a22a00a3b15c51ddb8ef36a0f34c6f59b290
FaizanHussainhsp <faizanhussainhsp@gmail.com>
2023-02-09 10:00:49 +0500

    first version

--- logic.js
+++ logic.js
@@ -7,74 +7,222 @@ const Updatebtn = document.querySelector(".Update");
 const deletebtn = document.querySelector(".delete");
 const GetBox = document.querySelector(".GetBox");
 const Updatedata = document.querySelector(".Updatedata");
-/////////////////////////replace Function
-function replace(value) {
-  data.forEach((data, index) => {
-    if (data === value) {
-      data[index] = value;
-    }
-  });
-}
+let InputSublist;
+const addbtnSublist = document.querySelector(".addbtnSublist");
+let ObjectRepresentation;
+let dataDiv;
+let printArrayData;
+let start = document.querySelector(".start");
+let end = document.querySelector(".end");
 
-//////////////////Insert Fnuction
+////////////////////test
+
+//////////////////Insert Function
 const addFunction = (e) => {
+  const ObjectPro = inputField.value;
   if (inputField.value) {
     ////////////////////////simple insertion/////////////////test
-    data.push(inputField.value);
+    data.push({ ObjectPro: ObjectPro.replace(/ /g, ""), list: [] });
+
     UI();
     inputField.value = "";
   }
+  ///////////////////////Style STATIC TEXT
+  start.style.display = "block";
+  end.style.display = "block";
+  printArrayData = document.querySelector(".printArrayData");
 };
-//////////////////////////Delete Function
+//////////////////////////Delete Main Object Function
 function deleteFunction(value, index) {
+  console.log("+++++++Chekc+++++++++", value, index);
   const delnode = document.getElementById(`${index}`);
   delnode.remove();
-  const delItem = data.filter((data) => data !== value);
+  const delItem = data.filter((data) => data.ObjectPro !== value);
   data = delItem;
+  if (data.length === 0) {
+    ///////////////////////Style STATIC TEXT
+    start.style.display = "none";
+    end.style.display = "none";
+  }
 }
 ////////////////////////Edit Function
-let holdvalue, holdelement, holdindex;
-function edit(holdValue, holdIndex) {
-  inputField.value = holdValue;
-  holdelement = document.getElementById(`${holdIndex}`);
+let holdvalue, holdelement, holdindex, holdobj, lastItem, holdinput;
+function edit(holdValue, holdIndex, obj, mainpoint) {
+  let parentNOde = document.getElementById(`hello${mainpoint}`).children;
+
+  for (const item of parentNOde) {
+    if (item.className === "setSpanData" && item.id == holdIndex) {
+      console.log(item);
+      holdelement = item;
+    }
+    if (item.id == `insertlistBox${mainpoint}`) {
+      console.log(item);
+      //  InputSublist.value = item;
+      let inputParent = item.children;
+      for (const item of inputParent) {
+        if (item.className === `InputSublist${mainpoint}`) {
+          item.value = holdValue;
+          holdinput = item;
+        }
+      }
+    }
+  }
+
+  // InputSublist.value = holdValue;
+  holdobj = obj;
+  // holdelement = document.getElementById(`${holdIndex}e`);
 
   holdindex = holdIndex;
   holdvalue = holdValue;
-  console.log(holdindex);
-  console.log(holdvalue);
-  console.log(holdelement);
+
+  // console.log("====================", holdvalue, holdindex, holdelement, obj);
 }
 
-//////////////////////UpDateData
-function upDateData() {
-  const temp = inputField.value;
+//////////////////////UpDateData in array
+function upDateData(pickDomObjeect) {
+  const temp = holdinput.value;
+  for (const item of data) {
+    if (item.ObjectPro === holdobj) {
+      item.list[holdindex] = temp;
+      let selectedParentList;
+      let parentObject = document.getElementById(
+        `hello${pickDomObjeect}`
+      ).children;
 
-  data.forEach((value, index) => {
-    if (value === holdvalue) {
-      data[index] = temp;
-      holdelement.textContent = temp;
-      inputField.value = "";
+      for (const item of parentObject) {
+        if (item.className === "setSpanData" && item.id === `${holdindex}`) {
+          console.log(item);
+          selectedParentList = item.children;
+          // let pickFChild = item.firstChild;
+
+          // pickFChild.textContent = "";
+          // pickFChild.textContent = temp;
+        }
+      }
+      for (const lists of selectedParentList) {
+        if (lists.id === `${holdindex}e`) {
+          lists.textContent = temp;
+        }
+      }
+
+      // holdelement.textContent = temp;
+      InputSublist.value = "";
     }
-  });
+  }
 }
 
+////////////////////add DATA into Object array
+function addDataInObject(catchObject, index) {
+  ObjectRepresentation = document.querySelector(
+    `.ObjectRepresentation${catchObject}`
+  );
+
+  InputSublist = document.querySelector(`.InputSublist${index}`);
+  for (const item of data) {
+    if (item.ObjectPro === catchObject) {
+      if (InputSublist.value === "") {
+        return;
+      }
+      {
+        console.log(InputSublist);
+        item.list.push(InputSublist.value);
+        InputSublist.value = "";
+        setspan(item.list, item.ObjectPro, index);
+      }
+    }
+  }
+}
+
+////////////////////////Event binding
 addBtn.addEventListener("click", addFunction);
 Updatedata.addEventListener("click", upDateData);
+
 /////////////////////////UI Update
 function UI() {
   let html;
 
   data.forEach((value, index) => {
     html = `<div class="datadiv" id="hello${index}">
-        <span class="list" id="h${index}">${value}</span>
-        <button class="Update" onclick="edit('${value}','h${index}')">
-          <img src="img/update.png" alt="" />
-        </button>
-        <button class="delete" onclick="deleteFunction('${value}','hello${index}')">
-          <img src="img/delete.png" alt="" />
-        </button>
-      </div>`;
+        <div class="ObjectRepresentation${value.ObjectPro}" id="data">
+          <span class="list" id="h${index}">${value.ObjectPro}</span>
+
+          <button
+            class="delete"
+            onclick="deleteFunction('${value.ObjectPro}','hello${index}')"
+          >
+            <img src="img/delete.png" alt="" />
+          </button>
+         
+        </div>
+      
+        <div class="insertlistBox" id="insertlistBox${index}">
+          <input type="text" class="InputSublist${index}" />
+          <button class="addbtnSublist"  onclick="addDataInObject('${value.ObjectPro}','${index}')">Insert</button>
+          <button class="addbtnSublist" onclick="upDateData('${index}')">
+   Update
+ </button>;
+          </div>
+      </div>
+      
+      `;
   });
-  GetBox.insertAdjacentHTML("afterbegin", html);
+
+  start.insertAdjacentHTML("afterend", html);
 }
-console.log(data);
+///////////////////////////////child list UI
+function setspan(value, objName, mainpoint) {
+  let html;
+  value.forEach((value, index) => {
+    html = `
+    <div class="setSpanData" id="${index}">
+    <span class="printArrayData${index}" id="${index}e">${value}</span>
+     <button
+            class="delete"
+            onclick="delSpanItem('${value}','${index}','${objName}','${mainpoint}')"
+          >
+            <img src="img/delete.png" alt="" />
+          </button>
+            <button
+             class="Update"
+           onclick="edit('${value}','${index}','${objName}','${mainpoint}')"
+           >
+             <img src="img/update.png" alt="" />
+           </button> 
+          </div>`;
+  });
+  console.log("========checkPoint========", html);
+  ObjectRepresentation.insertAdjacentHTML("afterend", html);
+}
+//////////////////////////////del obj list item
+function delSpanItem(value, index, objName, mainpoint) {
+  console.log("indexed value", index, "main point ", mainpoint);
+  // let cIndex = 1 + Number(index);
+
+  let parentNode = document.getElementById(`hello${mainpoint}`).children;
+  console.log(parentNode);
+  let deletedBox;
+  let deletedText;
+  for (const item of parentNode) {
+    if (item.className === "setSpanData" && item.id === index) {
+      deletedBox = item.children;
+      item.remove();
+    }
+  }
+  for (const item of deletedBox) {
+    if (item.id === `${index}e`) {
+      deletedText = item.textContent;
+    }
+  }
+  // console.log(parentNode[cIndex].remove());
+
+  data.forEach((checkobj) => {
+    if (checkobj.ObjectPro === objName) {
+      console.log("==========Condition is satisfied==============");
+
+      const copyArray = checkobj.list;
+      const filterData = copyArray.filter((data) => data !== deletedText);
+      checkobj.list = filterData;
+    }
+  });
+}
+////////////////////////////testing point
